a05714edb49db7413c40ff6b2d95e6ec049f6e6f
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2023-12-06 22:27:45 +0300

    Initial Commit

---
+++ TalkWithSoul/TalkWithSoulApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkWithSoulApp.swift
+//  TalkWithSoul
+//
+//  Created by İbrahim Erdogan on 6.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TalkWithSoulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
